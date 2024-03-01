4a2af5bdbc7d101fc4f7c7f2df9f32bf37466def
alvankrab <alvan.villacrosis@gmail.com>
2024-03-01 11:47:23 +0800

    updated file

--- obj/Debug/net8.0/SchedulingApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchedulingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6a8d62a42d139872f35f1eb5f07e3e58a2e78b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
